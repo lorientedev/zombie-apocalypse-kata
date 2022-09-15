f134ab72cd4a294a8d21a87ef244bff2c794aac8
Sonia Esteban <sesteban@idealista.com>
2022-09-15 17:30:46 +0200

    WE TRIED

--- Board.py
+++ Board.py
@@ -1,8 +1,11 @@
+from shutil import move
+
+
 class Board:
 
     size = 0
     board = [[]]
-    charcters = 
+    characters = {}
 
     def __init__(self, size):
         self.size = size
@@ -10,18 +13,35 @@ class Board:
 
     def add(self, board_element, x, y):
         self.board[x][y] = board_element
+        self.characters[board_element.name] = {"x":x, "y":y, "element": board_element}
 
     def executeInstruction(self, instruction):
         if instruction.type == "M":
-            
+            character_position = self.characters[instruction.character_name]
+            moveCharacter(character_position, instruction.direction)
         elif instruction.type == "P":
-
+            character_position = self.characters[instruction.character_name]
+            item_position = self.characters[instruction.item_name]
+            item_added = character_position.element.addItem(self, instruction.spot, item_position.element)
+            if item_added == True:
+                self.board[item_position.x][item_position.y] = 0
         elif instruction.type == "R":
+            
 
         elif instruction.type == "A":
 
 
-    def findCharacter(self, type):
+    def moveCharacter(self, character_position, movement):
+        self.board[character_position.x][character_position.y] = 0
+        if movement == "Up":
+            self.add(self, character_position.element, character_position.x, character_position.y+1)
+        elif movement == "Down":
+            self.add(self, character_position.element, character_position.x, character_position.y-1)
+        elif movement == "Right":
+            self.add(self, character_position.element, character_position.x+1, character_position.y)
+        elif movement == "Left":
+            self.add(self, character_position.element, character_position.x-1, character_position.y)
+       
 
     #Hay que definir que los zombies y supervivientes no puedan salir del mapa
 
--- Survivor.py
+++ Survivor.py
@@ -15,22 +15,52 @@ class Survivor:
         self.experience = experience
 
     def addItem(self, position, item):
+        success = False
         if position == "LeftHand":
             if (self.leftHandItem == None):
                 self.leftHandItem = item
+                success = True
             elif len(self.backpackItems) < 3:
                 self.backpackItems.append(self.leftHandItem)
                 self.leftHandItem = item
+                success = True
         elif position == "RightHand":
             if (self.rightHandItem == None):
                 self.rightHandItem = item
+                success = True
             elif len(self.backpackItems) < 3:
                 self.backpackItems.append(self.rightHandItem)
                 self.rightHandItem = item
+                success = True
         elif position == "Backpack":
             if len(self.backpackItems) < 3:
                 self.backpackItems.append(item)
-            
+                success = True
+
+        yield success
+
+    def moveItem(self, item_name, new_position):
+        item_found = None
+        if item_name == self.leftHandItem.name:
+            item_found = self.leftHandItem
+        elif item_name == self.rightHandItem.name:
+            item_found = self.rightHandItem
+        for item in self.backpackItems:
+            if item_name == item.name:
+                item_found = item
+        if item_found == None:
+            pass
+        if new_position == "LeftHand":
+            if (self.leftHandItem == None):
+                self.leftHandItem = item_found
+        elif new_position == "RightHand":
+            if (self.rightHandItem == None):
+                self.rightHandItem = item_found
+        elif new_position == "Backpack":
+            if len(self.backpackItems) < 3:
+                self.backpackItems.append(item_found)
+        elif new_position == "Floor":
+
         
 
 
